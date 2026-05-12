e0198f7942b3d8d6368dbef72a6505ad9ab3c90c
Cyvneil Enriquez <cyvenriquez1@gmail.com>
2026-05-13 03:50:21 +0800

    minor fix in About Us Page

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -1,3 +1,5 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import { FaShieldAlt, FaCheckDouble, FaFileContract } from 'react-icons/fa';
 import { brandConfig } from '../data/brandConfig';
 
 const AboutUs = () => {
